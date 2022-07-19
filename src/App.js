b16b77b8ca50b0b6967013b5192f51350245404c
VickyAruldoss <jamvicky3094@gmail.com>
2022-07-19 14:37:14 +0530

    undo the changes to test the git

--- src/App.js
+++ src/App.js
@@ -15,9 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React to test
-
-          
+          Learn React  
         </a>
       </header>
     </div>
